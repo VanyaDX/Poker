80ba8e4bddf317c05e8d15d50cbe80eaf48103b8
Nikolay Filchenko <finomen812@gmail.com>
2014-10-23 12:02:47 +0400

    More combinations

--- src/Rules.cpp
+++ src/Rules.cpp
@@ -1,18 +1,100 @@
 #include <algorithm>
+#include <set>
 #include "Rules.h"
 
-namespace Rules {
-	unsigned int getCombinationRank(std::vector <Card> const & cards, 
+namespace{
+	unsigned int checkPairs(std::vector <Card> const & cards,
 		std::shared_ptr<Player> const & player) {
-		std::vector<size_t> values(Card::ACE + 1, 0);
+		size_t values[Card::ACE + 1] = { 0 };
+		int rank = 0;
 		Card::Value highestCard = Card::NONE;
+		Card::Value secondCard = Card::NONE;
+
 		for (auto & c : cards) {
 			Card::Value curValue = c.look(player).second;
 			if (++values[curValue - Card::D2] == 2) {
+				if (curValue > highestCard) {
+					secondCard = highestCard;
+					highestCard = curValue;
+				}
+				else if (curValue > secondCard)
+					secondCard = curValue;
+			}
+		}
+
+		return (secondCard != Card::NONE ? Rules::TwoPairs : Rules::Pair) | highestCard << 8 | secondCard;
+	}
+	
+	unsigned int checkFlush(std::vector<Card> const & cards, std::shared_ptr<Player> const & player) {
+		Card::Value high = Card::NONE;
+		Card::Suit  suit = cards.cbegin()->look(player).first;
+
+		for (auto & c : cards) {
+			high = std::max(high, c.look(player).second);
+			if (c.look(player).first != suit)
+				return 0;
+		}
+		return high | Rules::Flush;
+	}
+
+	unsigned int checkStraight(std::vector<Card> const & cards,
+		std::shared_ptr<Player> const & player) {
+		size_t values[Card::ACE + 1] = { 0 };
+		for (auto & c : cards) {
+			++values[c.look(player).second - Card::D2];
+		}
+		
+		auto high = Card::NONE;
+		auto count = 0;
+
+		for (int i = Card::ACE; i >= Card::D2; --i) {
+			if (values[i - Card::D2]) {
+				if (high != Card::NONE) {
+					++count;
+				}
+				else {
+					high = (Card::Value)i;
+					count = 1;
+				}
+			}
+			else {
+				high = Card::NONE;
+			}
+		}
+
+		if (count == 5 || (count == 4 && high == Card::D5 && values[Card::ACE - Card::D2])) {
+			return Rules::Straight | high;
+		}
+
+		return 0;
+	}
+}
+
+namespace Rules {
+	unsigned int getCombinationRank(std::vector <Card> const & cards,
+		std::shared_ptr<Player> const & player) {
+		size_t values[Card::ACE + 1] = { 0 };
+		int rank = 0;
+		Card::Value highestCard = Card::NONE;
+	
+		if (unsigned flushResult = checkFlush(cards, player))
+			if (unsigned int straightResult = checkStraight(cards, player))
+				return straightResult & 0x00FFFFFF | Rules::StraightFlush;
+			else
+				return flushResult;
+
+		if (unsigned int straightResult = checkStraight(cards, player))
+			return straightResult;
+
+		for (auto & c : cards) {
+			Card::Value curValue = c.look(player).second;
+			if (++values[curValue - Card::D2] == 3) {
 				highestCard = std::max(highestCard, curValue);
 			}
 		}
-		
-        return Rules::Pair | highestCard;
-    }
+
+		return highestCard == Card::NONE ? checkPairs(cards, player) : Rules::Set | highestCard;
+	}
+
+	
 }
--- test/TestRules.hpp
+++ test/TestRules.hpp
@@ -3,7 +3,7 @@
 #include "Exceptions.h"
 
 
-BOOST_AUTO_TEST_SUITE(TestCard)
+BOOST_AUTO_TEST_SUITE(TestRules)
 
 
 BOOST_AUTO_TEST_CASE(TestOnePair)
@@ -22,6 +22,80 @@ BOOST_AUTO_TEST_CASE(TestTwoPair)
 	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D3)));
 	cards.emplace_back(std::move(Stack::makeCard(Card::Club, Card::D3)));
 	BOOST_CHECK_EQUAL(Rules::TwoPairs, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestSet)
+{
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D2)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Club, Card::D2)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::D2)));
+	BOOST_CHECK_EQUAL(Rules::Set, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestStraight)
+{
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D2)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Club, Card::D3)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::D4)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D5)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::D6)));
+	BOOST_CHECK_EQUAL(Rules::Straight, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestLowestStraight)
+{
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D2)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Club, Card::D3)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::D4)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D5)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::ACE)));
+	BOOST_CHECK_EQUAL(Rules::Straight, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestHighestStraight)
+{
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::ACE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Club, Card::KING)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::QUEEN)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::KNAVE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::D10)));
+	BOOST_CHECK_EQUAL(Rules::Straight, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestFlush)
+{
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::ACE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::KING)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::QUEEN)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::KNAVE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D2)));
+	BOOST_CHECK_EQUAL(Rules::Flush, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestStraightFlush)
+{
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::ACE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::KING)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::QUEEN)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::KNAVE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::D10)));
+	BOOST_CHECK_EQUAL(Rules::StraightFlush, Rules::getCombinationRank(cards) & 0xFF000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestFullHouse) {
+	std::vector<Card> cards;
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::ACE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::ACE)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::QUEEN)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Diamond, Card::QUEEN)));
+	cards.emplace_back(std::move(Stack::makeCard(Card::Heart, Card::QUEEN)));
+	BOOST_CHECK_EQUAL(Rules::FullHouse, Rules::getCombinationRank(cards) & 0xFF000000);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
